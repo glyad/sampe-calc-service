0f5b31be033ae319409f90ce846ad7f77a8de041
David Kossoglyad (Hire me!) <david@logoui.co.il>
2022-11-14 12:39:04 +0200

    Initialize develop

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,64 @@
+import { User } from '../model/user';
+import { autoinject, transient } from 'aurelia-dependency-injection';
+import { Param, Get, Post, Delete, Put, Body, JsonController, HttpCode, Header } from 'routing-controllers';
+import { DataService } from '../model/data.service';
+import { OpenAPI, ResponseSchema } from 'routing-controllers-openapi';
+
+@JsonController('/users')
+@autoinject
+@transient(UserController)
+@OpenAPI({
+  security: [{ basicAuth: [] }],
+})
+export class UserController {
+
+  // eslint-disable-next-line no-unused-vars
+  constructor(private dataService: DataService) {
+  }
+
+  @Get('/')
+  @HttpCode(200)
+  @Header('Cache-Control', 'none')
+  @Header('X-Powered-By', 'Kossoglyad')
+  @OpenAPI({ summary: 'Return a list of users' })
+  @ResponseSchema(User, {
+    contentType: 'application/json',
+    description: 'A list of user objects',
+    isArray: true,
+    statusCode: '200'})
+  public async get(): Promise<User[]> {
+    const users = await this.dataService.getUsers();
+    return users;
+  }
+
+  @Get('/:id')
+  @OpenAPI({ summary: 'Return a single user' })
+  public async getOne(@Param('id') id: number): Promise<User> {
+    return (await this.dataService.getUsers()).find(user => id == user.id);
+  }
+
+  @HttpCode(201)
+  @Post('/')
+  @OpenAPI({ summary: 'Create a new user' })
+  @ResponseSchema(User, {
+    contentType: 'application/json',
+    description: 'A newly created user object',
+    statusCode: '201',
+    isArray: false
+  })
+  public post(@Body({validate: true}) user: User): User {
+    return { ...user };
+  }
+
+  @Put('/')
+  // eslint-disable-next-line no-unused-vars
+  put(@Body({validate: true}) user: User) {
+    return 'Updating a user...';
+  }
+
+  @Delete('/:id')
+  // eslint-disable-next-line no-unused-vars
+  delete(@Param('id') id: number) {
+    return 'Removing user...';
+  }
+}
